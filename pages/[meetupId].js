ced8c5c2b07057d4f855c2744015a246200a6a9e
unknown <oguzhandemiraslan@gmail.com>
2022-09-25 14:13:27 -0400

    detail page updated

--- pages/[meetupId].js
+++ pages/[meetupId].js
@@ -1,1 +1,15 @@
+import MeetupDetail from "../components/meetups/MeetupDetail";
+
 //ourdomain.com/mettingId
+function MeetUpDetails() {
+  return (
+    <MeetupDetail
+      image="https://www.nordantech.com/media/pages/blog/community/8-tipps-fuer-ein-erfolgreiches-meeting/00022d9063-1643812301/meeting-tipps-erfolgreich-1200x630.jpg"
+      title="first meetup"
+      address="some street,some city"
+      description="this is our first meetup"
+    />
+  );
+}
+
+export default MeetUpDetails;
